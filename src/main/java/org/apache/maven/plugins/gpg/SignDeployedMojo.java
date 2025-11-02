505669e09421f4511c077fb85f8f9e0a8f093250
Sylwester Lachiewicz <slachiewicz@apache.org>
2025-11-02 23:16:30 +0000

    `PlexusFileUtils` Refaster recipes
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.codehaus.plexus.PlexusFileUtilsRecipes?organizationId=YTA5ODhiOTYtNDI5OS00OGY3LTg0NjctNGZiNmI4OTY1ZjY3
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/apache/maven/plugins/gpg/SignDeployedMojo.java
+++ src/main/java/org/apache/maven/plugins/gpg/SignDeployedMojo.java
@@ -36,11 +36,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.codehaus.plexus.util.FileUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
